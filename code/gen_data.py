6adbd71c7cdc5b17fd63659e27a9ca7c4bba40ed
carlos <carloscerlira@gmail.com>
2020-12-23 11:53:19 -0600

    update gen_data

--- code/gen_data.py
+++ code/gen_data.py
@@ -1,17 +1,18 @@
 import pandas as pd 
 import numpy as np 
 import matplotlib.pyplot as plt 
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
 plt.style.use("seaborn")
 
-
-def gen_data():
+def gen_data_cdmx():
     url = "https://raw.githubusercontent.com/carloscerlira/Datasets/master/COVID19CDMX/train.csv"
     df = pd.read_csv(url);
 
-    columns = ['fechreg', 'sexo', 'fecdef', 'intubado', 'digcline', 'edad', 'estaemba', 'fiebre', 'tos', 'odinogia', 'disnea', 'irritabi', 'diarrea', 
+    columns = ['tipacien', 'fechreg', 'sexo', 'fecdef', 'intubado', 'digcline', 'edad', 'estaemba', 'fiebre', 'tos', 'odinogia', 'disnea', 'irritabi', 'diarrea', 
             'dotoraci','calofrios', 'cefalea','mialgias', 'artral', 'ataedoge', 'rinorrea', 'polipnea', 'vomito','dolabdo', 'conjun', 	'cianosis',
             'inisubis','diabetes', 'epoc', 'asma', 'inmusupr', 'hiperten', 'vihsida', 'otracon', 'enfcardi', 'obesidad', 'insrencr',
-            'tabaquis', 'resdefin', 'tipacien']
+            'tabaquis', 'resdefin']
 
     dfmini = df.loc[:,columns]
     dfmini = dfmini[dfmini['resdefin'].isin(['NEGATIVO','SARS-CoV-2'])] 
@@ -31,12 +32,12 @@ def gen_data():
 
     dfmini['edad']= (dfmini['edad']-dfmini['edad'].mean())/dfmini['edad'].std()
 
-    col_com  = ['sexo', 'tipacien', 'edad', 'diabetes',	'epoc', 'asma',	'inmusupr', 'hiperten', 'vihsida', 'otracon', 'enfcardi', 'obesidad', 'insrencr',
-            'tabaquis', 'resdefin', 'fecdef']
+    col_com  = ['fecdef', 'sexo', 'tipacien', 'edad', 'diabetes', 'epoc', 'asma', 'inmusupr', 'hiperten', 'vihsida', 'otracon', 'enfcardi', 'obesidad', 'insrencr',
+            'tabaquis', 'resdefin']
 
-    col_sin = ['sexo', 'tipacien', 'intubado', 'digcline', 'edad', 'estaemba', 'fiebre', 'tos', 'odinogia', 'disnea', 'irritabi', 'diarrea', 
+    col_sin = ['resdefin', 'sexo', 'tipacien', 'intubado', 'digcline', 'edad', 'estaemba', 'fiebre', 'tos', 'odinogia', 'disnea', 'irritabi', 'diarrea', 
             'dotoraci','calofrios', 'cefalea', 'mialgias', 'artral', 'ataedoge', 'rinorrea', 'polipnea', 'vomito', 'dolabdo', 'conjun', 'cianosis',
-            'inisubis', 'resdefin']
+            'inisubis']
 
     df_com = dfmini.loc[:,col_com]
     df_com = df_com[df_com['resdefin']==1]
@@ -50,3 +51,40 @@ def gen_data():
     df_hosp.drop('intubado', axis=1, inplace=True)
     
     return df_com, df_sin, df_hosp
+
+def gen_data_mx():
+    return df_com, df_hosp
+
+def get_conf(X, y, classifier):
+    conf = [[0,0],[0,0]]
+    for clf in [0, 1]:
+        for pred_clf in [0, 1]:
+            X_clf = X[y == clf]
+            y_clf = y[y == clf]
+            y_pred = classifier.predict(X_clf)
+            cnt = len(y_pred[y_pred == pred_clf])
+            prob = cnt/len(y_clf)
+            conf[clf][pred_clf] = prob
+    TP, FP, FN, TN = conf[0][0], conf[0][1], conf[1][0], conf[1][1]
+    acc = (TP+TN)/(TP+TN+FP+FN)
+    prec = TP/(TP+FP)
+    fm = 2*TP/(2*TP+FP+FN)
+    recall = TP/(TP+FN)
+    print("Accuaracy: ", acc)
+    print("Precision: ", prec)
+    print("f-measure: ", fm)
+    print("Recall: ", recall)
+    return conf 
+
+def predict(X, y, gen_clf):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=1)
+    X_train = np.concatenate((X_train[y_train==0][:len(y_train[y_train==1])], X_train[y_train==1]))
+    y_train = np.concatenate((y_train[y_train==0][:len(y_train[y_train==1])], y_train[y_train==1]))
+    
+    clf = gen_clf()
+    y_pred = clf.fit(X_train, y_train)
+    
+    print(clf.score(X_test, y_test))
+    conf = get_conf(X_test, y_test, clf)
+    for row in conf:
+        print(row)
